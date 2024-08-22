43c5284e96fedc1d6c6cdb7e04b6ead56a0c9aba
chirag.sharma <chirag.sharma@growthnatives.co>
2024-08-23 01:47:47 +0530

    chnages done

--- app/src/androidTest/java/com/example/dobcalculator/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/dobcalculator/ExampleInstrumentedTest.kt
@@ -20,5 +20,5 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.dobcalculator", appContext.packageName)
-    }
+    }s
 }
